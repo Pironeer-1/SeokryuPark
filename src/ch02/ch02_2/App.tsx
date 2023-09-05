136580b5741abd42f763dd861d742be6ec7df69e
pomiryu <daniel231963@naver.com>
2023-09-05 16:55:46 +0900

    02-2 JSX 구문탐구

---
+++ src/ch02/ch02_2/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+// export default function Box() {
+//   return (
+//     <div>hello</div>
+//   )
+// }
+
+export default function App() {
+  const children = new Array(100).fill(null).map((notUsed, index) => <Text>Hello World! {index}</Text>)
+  return (
+    <SafeAreaView>
+      {children}
+    </SafeAreaView>
+  )
+}
