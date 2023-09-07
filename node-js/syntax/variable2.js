2cfd93f992b986e7cdc4b10e27161dd5e469bc2e
pomiryu <daniel231963@naver.com>
2023-09-07 16:42:28 +0900

    7.2 변수 활용

---
+++ node-js/syntax/variable2.js
@@ -0,0 +1,4 @@
+
+let name = 'Daniel';
+let letter = "Lorem "+ name +" Ipsum is simply dummy text "+ name +" of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s." + name; 
+console.log(letter);
