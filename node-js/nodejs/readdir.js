f38573d76778aa5f5d1794dd8417681f591385a1
pomiryu <daniel231963@naver.com>
2023-09-08 21:58:26 +0900

    파일목록 알아내기

---
+++ node-js/nodejs/readdir.js
@@ -0,0 +1,9 @@
+const testFolder = './data';
+const fs = require('fs');
+
+fs.readdir(testFolder, (err, filelist) => {
+  console.log(filelist);
+  filelist.forEach(file => {
+    console.log(file);
+  });
+});
