127867a1a4d388428d77c9a63a4e00a0e691c572
pomiryu <daniel231963@naver.com>
2023-09-16 03:48:53 +0900

    37. 글 삭제 - 삭제버튼 구현

--- node-js/main.js
+++ node-js/main.js
@@ -64,7 +64,12 @@ var app = http.createServer(function(request,response){
               var title = queryData.id;
               var template = templateHTML(title, list,
                 `<h2>${title}</h2><p>${description}</p>`,
-                `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+                `<a href="/create">create</a>
+                <a href="/update?id=${title}">update</a>
+                <form action="delete_process" method="post" onsubmit="return confirm('정말로 삭제하시겠습니까?');">
+                    <input type="hidden" name="id" value="${title}">
+                    <input type="submit" value="delete">
+                </form>`
                 );
               response.writeHead(200);
               response.end(template);
