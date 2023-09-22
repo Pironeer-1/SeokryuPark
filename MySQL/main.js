d2bb9f878dbcf64167c26b309d3799601b6b4386
pomiryu <daniel231963@naver.com>
2023-09-22 15:55:10 +0900

    04. mysql 모듈을 이용한 홈페이지 구현

--- MySQL/main.js
+++ MySQL/main.js
@@ -6,6 +6,15 @@ var qs = require('querystring');
 var template = require('./lib/template.js');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
+var mysql = require('mysql');
+
+var db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '111111',
+    database : 'opentutorials'
+})
+db.connect();
 
 var app = http.createServer(function(request, response) {
     var _url = request.url;
@@ -14,14 +23,26 @@ var app = http.createServer(function(request, response) {
 
     if(pathname === '/') {
         if(queryData.id === undefined) {
-            fs.readdir('./data', function(error, filelist) {
+            // fs.readdir('./data', function(error, filelist) {
+            //     var title = 'Welcome';
+            //     var description = 'Hello, Node.js';
+            //     var list = template.list(filelist);
+            //     var html = template.HTML(title, list,
+            //         `<h2>${title}</h2><p>${description}</p>`,
+            //         `<a href="/create">create</a>`
+            //     );
+            //     response.writeHead(200);
+            //     response.end(html);
+            // });
+            db.query(`SELECT * FROM topic`, function(error, topics) {
                 var title = 'Welcome';
                 var description = 'Hello, Node.js';
-                var list = template.list(filelist);
+                var list = template.list(topics);
                 var html = template.HTML(title, list,
-                    `<h2>${title}</h2><p>${description}</p>`,
+                    `<h2>${title}</h2>${description}`,
                     `<a href="/create">create</a>`
-                );
+                    );
+                console.log(topics);
                 response.writeHead(200);
                 response.end(html);
             });
